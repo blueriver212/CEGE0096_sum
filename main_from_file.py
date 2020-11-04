0d159c5a8459a7dd2d62a67b70c3f8ae6e9ba0a4
blueriver212 <indy.brownhall@gmail.com>
2020-11-04 11:55:24 +0000

    Fix: Now added the correct code to calculate MBR on polygon, and test vs input csv

--- main_from_file.py
+++ main_from_file.py
@@ -1,7 +1,7 @@
 from plotter import Plotter
 
 
-def import_polygon_points(path):
+def import_points(path):
     """
     Imports a CSV from chosen file path, then outputs an ID, X, Y list
     :param path is a directory to the csv file containing polygon points:
@@ -53,14 +53,38 @@ class MinBR:
         mbr_y = [self.min_y, self.min_y, self.max_y, self.max_y]
         return mbr_x, mbr_y
 
+class InsideMBR:
+
+    def __init__(self, x_points, y_points, mbr_xs, mbr_ys):
+        self.x_points = x_points
+        self.y_points = y_points
+        self.__mbr_xs = mbr_xs
+        self.__mbr_ys = mbr_ys
+
+        #get the min and max of the MBR polygon
+        self.max_xmbr = max(self.__mbr_xs)
+        self.min_xmbr = min(self.__mbr_xs)
+        self.max_ymbr = max(self.__mbr_ys)
+        self.min_ymbr = min(self.__mbr_ys)
+
     def is_inside(self):
         kind = []
-        for i in range(len(self.__xs)):
-            if self.min_x <= self.__xs[i] <= self.max_x and self.min_y <= self.__ys[i] <= self.max_y:
+        for i in range(len(self.x_points)):
+            if self.min_xmbr <= self.x_points[i] <= self.max_xmbr and self.min_ymbr <= self.y_points[i] <= self.max_ymbr:
                 kind.append('inside')
             else:
                 kind.append('outside')
-        return self.__xs, self.__ys, kind
+        return self.x_points, self.y_points, kind
+
+class RCA(MinBR):
+
+    def __init__(self, xs, ys, poly):
+        self.___xs = xs
+        self.__ys = ys
+        self.__poly = poly
+
+
+
 
 def main():
     # print("read polygon.csv")
@@ -71,29 +95,37 @@ def main():
     plotter = Plotter()
 
     #Import the points and add to individual lists
-    point_data = import_polygon_points('polygon.csv')
-    x = point_data[1]
-    y = point_data[2]
+    polygon_points = import_points('polygon.csv')
+    x_poly = polygon_points[1]
+    y_poly = polygon_points[2]
 
-    # plot the Polygon
-    plotter.add_polygon(x, y)
+    #plot the Polygon
+    plotter.add_polygon(x_poly, y_poly)
 
-    # plot the MBR polygon
-    imp = MinBR(x, y)
+    #calculate and plot the MBR polygon
+    imp = MinBR(x_poly, y_poly)
     mbr = imp.min_max()
+    print(mbr)
     plotter.add_poly_outline(mbr[0], mbr[1])
 
-    #calculate if the points are inside, outside or on the boundary
-    mbr = MinBR(x, y)
-    mbr_ = mbr.is_inside()
+
+    # import the individual points
+    points = import_points('input.csv')
+    x = points[1]
+    y = points[2]
+
+    #Test whether these points are within the Polygon's MBR
+    pmbr = InsideMBR(x, y, mbr[0], mbr[1])
+    mbr_ = pmbr.is_inside()
     MinBR_x = mbr_[0]
     MinBR_y = mbr_[1]
     MinBR_kind = mbr_[2]
 
+
     for i in range(len(MinBR_x)):
         plotter.add_point(MinBR_x[i], MinBR_y[i], MinBR_kind[i])
 
-    plotter.show()
+    #plotter.show()
 
 if __name__ == "__main__":
     main()
