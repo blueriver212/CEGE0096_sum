e71affdcab3310c1741559a86d4d6d64301f48bd
blueriver212 <indy.brownhall@gmail.com>
2020-11-13 22:42:33 +0000

    Feature: Added a print statment, telling the user where their point is

--- main_from_user.py
+++ main_from_user.py
@@ -14,7 +14,7 @@ def main():
 
     # first create the mbr from the polygon
     mbr = MBR(res)
-    poly_mbr = mbr.min_max()
+    poly_mbr = mbr.mbr_coords()
     print(poly_mbr)
     plotter.add_poly_outline(poly_mbr[0], poly_mbr[1])
     #
@@ -34,18 +34,22 @@ def main():
         plotter.add_point(is_outside_mbr[0][0], is_outside_mbr[0][1], 'outside')
 
     # If inside MBR check if a vertex or on boundary
-    inside_mbr_points = Inside(is_inside_mbr, poly)
+    inside_mbr_points = Boundary(is_inside_mbr, poly)
     on_vertex = inside_mbr_points.on_vertex()
 
+    classification = []
     # plot point if on vertex
     if len(on_vertex) != 0:
         plotter.add_point(on_vertex[0][0], on_vertex[0][1], 'boundary')
+        classification.append('boundary')
     else:
         point_on_line = inside_mbr_points.points_on_line()
 
         # check if the point is on boundary
         if len(point_on_line[0]) != 0:
             plotter.add_point(point_on_line[0][0][0], point_on_line[0][0][1], 'boundary')
+            classification.append('boundary')
+
         else:
             not_classified = RayCasting(point, poly)
             final_round = not_classified.rca()
@@ -55,9 +59,15 @@ def main():
             # Plot if inside or outside polygon
             if len(inside_poly) !=0:
                 plotter.add_point(inside_poly[0][0], inside_poly[0][1], 'inside')
+                classification.append('inside')
             else:
                 plotter.add_point(outside_poly[0][0], outside_poly[0][1], 'outside')
+                classification.append('outside')
 
+    if classification[0] == 'boundary':
+        print(f'Your chosen point is on the {classification[0]} of the polygon')
+    else:
+        print(f'Your chosen point is {classification[0]} the polygon')
     plotter.show()
 
 
